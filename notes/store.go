093cde96ee81ea634791538cf5e848605e76d09e
Dallas S. <andrewdallassherman@gmail.com>
2025-06-07 23:17:53 +0900

    Starting CRUD and storage functions, started notes tui model
    notes/store.go: Init function that will search for the user's path and
    then find Udon's storage directory; if it cannot be found it will create
    the necessary directories.
    notes/note.go: Note struct implementation
    notes.go: created an inital model for bubbletea, created the Init
    function for notes views.

--- notes/store.go
+++ notes/store.go
@@ -1,1 +1,32 @@
 package notes
+
+import (
+	"fmt"
+	"os"
+	"os/user"
+	"path/filepath"
+)
+
+type Store struct {
+	notesDir string
+}
+
+// TODO Take a look at permissions and path for notes (I'm not a big fan of storage in Documents)
+func (s *Store) Init() error {
+	// Get home directory
+	usr, err := user.Current()
+	if err != nil {
+		return nil
+	}
+
+	// Set where Udon will store and retrieve notes locally
+	// TODO change this for a configuration approach by the user
+	s.notesDir = filepath.Join(usr.HomeDir, "Documents", "udon")
+
+	// Create if does not exist
+	if err := os.MkdirAll(s.notesDir, 0755); err != nil {
+		fmt.Printf("There was an error creating note path: %s", err)
+	}
+
+	return nil
+}
--- tui/notes.go
+++ tui/notes.go
@@ -1,1 +1,25 @@
 package tui
+
+import (
+	"github.com/charmbracelet/bubbles/textarea"
+	"github.com/charmbracelet/bubbles/textinput"
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+// FIXME this is borrowed code from a previous project, need to make sure the bubbletea structure will be the same before commiting to this model approach
+type model struct {
+	state uint
+	// store     *store
+	// notes     []Note
+	// currNote  Note
+	listIndex int
+	textarea  textarea.Model
+	textinput textinput.Model
+}
+
+func NewModel(store *Store) model {
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
